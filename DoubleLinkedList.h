3fc239a042f19361c3a45da57687198e0d725712
Yongha <loha31707@gmail.com>
2023-03-27 02:19:01 +0900

    DoubleLinkedList - Add, Print

---
+++ DoubleLinkedList.h
@@ -0,0 +1,12 @@
+#pragma once
+
+struct Node
+{
+	Node* prevnode;
+	Node* nextnode;
+	int data;
+};
+
+Node* Add(Node* node);
+void Remove(int node);
+void PrintNodes();
---
+++ main.cpp
@@ -0,0 +1,40 @@
+﻿#include <assert.h>
+#include <iostream>
+#include <string.h>
+
+#include "DoubleLinkedList.h"
+
+using namespace std;
+
+int main()
+{
+	string input;
+	int num;
+
+	while (true)
+	{
+		cin >> input;
+		cout << '\n';
+
+		if (input == "1") {
+			cout << "추가할 정수 값을 입력해주세요";
+			cin >> num;
+			cin.clear();
+
+			Node* node = (Node*)malloc(sizeof(Node));
+
+			assert(node != nullptr);
+
+			node->data = num;
+			Add(node);
+			continue;
+		}
+
+		if (input == "2")
+		{
+			PrintNodes();
+		}
+	}
+
+	return 0;
+}
